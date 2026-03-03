dca762f544ff088e1399d8561bee52cba8cd2d1f
Inamullah <sheenzada7@gmail.com>
2026-03-02 18:53:44 -0800

    react

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -22,21 +22,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { DashboardLayout } from './layouts/DashboardLayout';
 import { routes } from './routes';
-import Tables from './Tables/Tables'
+// import Tables from './Tables/Tables'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<Tables />}> */}
-        <Route path='/' element={<Tables />}>
+        {/* <Route path='/' element={<Tables />}> */}
         <Route path="/" element={<DashboardLayout />}>
 
           {routes.map((route) => (
             <Route key={route.path} path={route.path} element={route.element} />
           ))}
           </Route>
-        </Route>
+        {/* </Route> */}
       </Routes>
     </BrowserRouter>
   );
