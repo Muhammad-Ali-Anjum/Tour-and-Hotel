2056f0076aab25542b796c632aeaee7118248dec
Inamullah <sheenzada7@gmail.com>
2026-03-02 18:42:12 -0800

    react

--- Frontend/src/store/slices/index.ts
+++ Frontend/src/store/slices/index.ts
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import authReducer from './authSlice';
 import packagesReducer from './packagesSlice';
-// import other reducers
 
 const rootReducer = combineReducers({
   auth: authReducer,
   packages: packagesReducer,
-  // add others
 });
 
 export default rootReducer;
